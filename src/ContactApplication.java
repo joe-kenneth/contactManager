1812cc63be08271858d82b04b468edf67b2d788f
Joe Garcia <gijoe250@gmail.com>
2023-02-27 11:35:51 -0600

    adding add concact, display contact, exit, and incorrect input

--- src/ContactApplication.java
+++ src/ContactApplication.java
@@ -2,19 +2,74 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class ContactApplication {
     public static void main(String[] args){
         Path contactsPath = Paths.get("data", "contacts.txt");
-        List<String> contactList;
+        List<String> contactFile;
+        ArrayList<Contact> contactList = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
+        int userInput = 0;
+
         try {
-            contactList = Files.readAllLines(contactsPath);
-            for (int i = 0; i < contactList.size(); i += 1) {
-                System.out.println((i + 1) + ": " + contactList.get(i));
+            contactFile = Files.readAllLines(contactsPath);
+
+            for (String contactInfo : contactFile){
+                String [] result = contactInfo.split(",");
+                Contact contactTemp = new Contact(result[0], result[1]);
+                contactList.add(contactTemp);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+
+        while (true){
+            System.out.print("""
+                    
+                    1. View contacts.
+                    2. Add a new contact.
+                    3. Search a contact by name.
+                    4. Delete an existing contact.
+                    5. Exit.
+                    Enter an option (1, 2, 3, 4 or 5):
+                    """);
+
+            try {
+                userInput = Integer.parseInt(scan.nextLine());
+            }
+            catch (Exception e){
+                System.out.println("That's not even a Int!. Enter an Int!!!!!!");
+                continue;
+            }
+            if (userInput == 1){
+                System.out.println("Name | Phone Number");
+                System.out.println("----------------------------");
+                for (Contact contact : contactList){
+                    System.out.println(contact.getName() + " | " + contact.getPhoneNumber());
+                }
+            } else if (userInput == 2) {
+                System.out.println("Enter your name");
+                String name = scan.nextLine();
+                System.out.println("Enter your 10 digit phone number");
+                String phoneNumber = scan.nextLine();
+                String phoneNumDashes = phoneNumber.substring(0,3) + "-" + phoneNumber.substring(3,6) + "-" + phoneNumber.substring(6);
+                System.out.println(name);
+                Contact contact = new Contact(name, phoneNumDashes);
+                contactList.add(contact);
+            } else if (userInput == 3) {
+
+            }else if (userInput == 4) {
+
+            }else if (userInput == 5) {
+                System.out.println("You are exiting the program. Goodbye!");
+                break;
+            }
+            else {
+                System.out.println("This is not a correct input. Try again punk!");
+            }
         }
     }
 }
